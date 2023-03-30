b64c1f29b5188c3f0354c242fcd1ce2d67455fc7
johnari21 <johnarinata21@gmail.com>
2023-03-30 08:39:12 +0700

    create looping and conditional program

--- main.go
+++ main.go
@@ -1,1 +1,22 @@
 package main
+
+import "fmt"
+
+func main() {
+
+	for i := 0; i < 5; i++ {
+		fmt.Printf("Nilai i = %v\n", i)
+	}
+
+	for j := 0; j < 11; j++ {
+
+		if j == 5 {
+
+			for idx, val := range "САШАРВО" {
+				fmt.Printf("character %#U starts at byte position %d\n", val, idx)
+			}
+		}
+
+		fmt.Printf("Nilai j = %v\n", j)
+	}
+}
